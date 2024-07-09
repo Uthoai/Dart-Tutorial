9d077e0e7cb66fa0da027737e5ff6f86e2b06427
uthoai <uthoaimarma597@gmail.com>
2024-07-09 23:37:33 +0600

    dart basic, collection

---
+++ bin/01.dart_basic/DartBasic.dart
@@ -0,0 +1,68 @@
+void main(){
+
+  // Dart Basic
+
+  /*var name = "Uthoai Marma";
+  var isStudent = true;
+  print(name);
+  print("Student: $isStudent");*/
+
+  //dynamic keyword used variable can accept any type of value
+  /*dynamic SS = 10;
+  SS = "Get";
+  print(SS);*/
+
+
+  //Collection
+
+  //List or Growable List
+  /*var city = <String>["Dhaka", "Chittagong","Beijing","Mumbai","Bangkok"];
+  print(city);
+
+  city[4]= "Jakarta";
+  city.add("Dubai");
+  var city2 = ["New York", "Berlin","London","Karachi","Gangtok"];
+  city.addAll(city2);       // or city.addAll(["New York", "Berlin","London","Karachi","Gangtok"]);
+
+  city.insert(1, "Khulna");
+  print(city);
+
+  city.remove("Dhaka");
+  city.removeAt(1);
+  city.removeLast();
+  city.removeRange(0, 3);
+  print(city);*/
+
+  //Map
+  /*var studentData = {"name": "Uthoai Marma", "age": 25, "dept": "CSE"};
+  print(studentData["name"]);
+  print(studentData["dept"]);
+
+  studentData.forEach((key, value) {
+    print(value);
+  });
+  // or
+  var employeeData = new Map();
+  employeeData['name'] = "Uthoai";
+  employeeData['id'] = "EMP01";
+  print(employeeData['name']);*/
+
+  //Set
+  //Set don't contain duplicate value
+  /*Set<String> id = {'EAP01', 'EAP02', 'EAP03', 'EAP03'};
+  print(id);
+  id.add('EAP01');
+  id.add('EAP04');
+  print(id);
+  print(id.elementAt(1));
+  print(id.elementAtOrNull(10));
+
+  id.forEach((element) {
+    print(element);
+  });*/
+
+  //Dart control flow statement
+
+}
+
+
